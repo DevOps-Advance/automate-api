cfa68885439b0e1cffcd70aedbd3c6737edd80aa
MuyleangIng <muyleanging@gmail.com>
2023-11-07 10:02:35 +0700

    first update

---
+++ src/test/java/co/istad/automate/AutomateApplicationTests.java
@@ -0,0 +1,13 @@
+package co.istad.automate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
